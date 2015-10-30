f387210f0a46b4a0ac3e13c46d29749062adfcf9
Clad07 <clad-07@live.fr>
2015-10-30 17:36:47 +0100

    Signed-off-by: Clad07 <clad-07@live.fr>

--- app.js
+++ app.js
@@ -129,10 +129,10 @@ io.sockets.on('connection', function (socket, pseudo) {
 			  client
 				.query("SELECT * FROM historiquechat WHERE pseudo <> '' ORDER BY id DESC LIMIT 25;")
 				.on('row', function(row) {
-					//for(var key = row.length-1; key>=0; key--){
-						console.log({pseudo: row[key].pseudo, message: row[key].text, date: moment(row[key].date).format("HH:mm:ss")});
-						socket.emit('message', {pseudo: row[key].pseudo, message: row[key].text, date: moment(row[key].date).format("HH:mm:ss")/*new Date(row[key].date).toLocaleTimeString()*/});
-					//}
+					console.log(row);
+					socket.emit('message', {pseudo: row[key].pseudo, message: row[key].text, date: moment(row[key].date).format("HH:mm:ss")/*new Date(row[key].date).toLocaleTimeString()*/});
+				})
+				.on('end', function(){
 					socket.emit('nouveau_client', pseudo, null, moment(dat).format("HH:mm:ss"));
 				});
 			});
