969642121a4305e0b22f5549ecc327be647e60b9
Clad07 <clad-07@live.fr>
2015-10-30 17:10:45 +0100

    test copy fct

--- app.js
+++ app.js
@@ -128,8 +128,15 @@ io.sockets.on('connection', function (socket, pseudo) {
 
 			  client
 				.query("SELECT * FROM historiquechat WHERE pseudo <> '' ORDER BY id DESC LIMIT 25;")
-				.on('row', processResult(row));
-			});
+				.on('row', function(row) {
+					console.log(row);
+					console.log("\n\n\n" + (row.length-1) + "\n\n\n");
+					for(var key = row.length-1; key>=0; key--){
+						console.log({pseudo: row[key].pseudo, message: row[key].text, date: moment(row[key].date).format("HH:mm:ss")});
+						socket.emit('message', {pseudo: row[key].pseudo, message: row[key].text, date: moment(row[key].date).format("HH:mm:ss")/*new Date(row[key].date).toLocaleTimeString()*/});
+					}
+					socket.emit('nouveau_client', pseudo, null, moment(dat).format("HH:mm:ss")););
+				});
 		}
     });
 	
